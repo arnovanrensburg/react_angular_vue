633b79a050bdf12f2100ada394a5d4723cb87882
ARNOR-E6540\Arno van Rensburg <arno@vastech.co.za>
2018-07-31 10:50:26 +0200

    React tutorial (Lecture 78)

--- tutorial_react/script.js
+++ tutorial_react/script.js
@@ -1,13 +1,27 @@
 
-
-function App(props) {
-    return (
-        <div>
-            <p>{props.name}</p>
-            <button>Change Name</button>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+        };
+    }
+    changeName() {
+        this.setState({
+            name:'Anna',
+        });
+    }
+    render() {
+        return (
+            <div>
+                <p>{this.state.name}</p>
+                <button onClick={this.changeName.bind(this)}>Change Name</button>
+            </div>
+        );
+    }
 }
+
+
 
 //const element = React.createElement('p',null,'Max');
 ReactDOM.render(<App name="Max"/>, document.querySelector('#app'));
