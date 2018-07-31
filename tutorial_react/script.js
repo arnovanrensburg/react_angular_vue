f8e64cd4c27f7b8ca389b8c01ac2876ddbfc5319
ARNOR-E6540\Arno van Rensburg <arno@vastech.co.za>
2018-07-31 11:17:16 +0200

    React tutorial (Lecture 81)

--- tutorial_react/script.js
+++ tutorial_react/script.js
@@ -1,9 +1,9 @@
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             name: props.name,
+            elements: []
         };
     }
 
@@ -13,22 +13,33 @@ class App extends React.Component {
         });
     }
 
+    addElement(element) {
+        const oldElements = this.state.elements;
+        this.setState({
+            elements: oldElements.concat(oldElements.length+1)
+        });
+
+    }
+
     render() {
         let updateParagraph = '';
         if (this.state.name != this.props.name) {
             updateParagraph = <p>Name Updated</p>;
         }
+        let list = this.state.elements.map(
+            (el) -> return <li>{el}</li>
+    );
         return (
             <div>
                 <p>{this.state.name}</p>
                 <button onClick={this.changeName.bind(this)}>Change Name</button>
+                <button onClick={this.addElement.bind(this)}>Add Element</button>
                 {updateParagraph}
+                <ul>
+                    {list}
+                </ul>
+
             </div>
         );
     }
 }
-
-
-
-//const element = React.createElement('p',null,'Max');
-ReactDOM.render(<App name="Max"/>, document.querySelector('#app'));
